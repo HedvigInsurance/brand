c8ee42e2f6f58dda9dec37376959acde35f16ef4
Oscar Nylander <oscar@hedvig.com>
2018-09-13 17:29:36 +0200

    Shake it up a bit

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { colors } from './colors'
-import { fonts } from './fonts/web'
+import { fonts } from './fonts'
 
 export {
   colors,
--- native.ts
+++
@@ -1,4 +0,0 @@
-import { colors } from './colors'
-import { fonts } from './fonts/native'
-
-export { colors, fonts }
