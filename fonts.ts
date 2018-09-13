497e5a215f5b0154dce1401b3e5ae93eb25b33fd
Oscar Nylander <oscar@hedvig.com>
2018-09-13 11:26:47 +0200

    Support multiple platforms for fonts
    - The names are different on Native and Web
    - We use `react-primitives` to determine the names to export

--- fonts.ts
+++ fonts.ts
@@ -1,5 +1,19 @@
-export const fonts = {
-  MERRIWEATHER: "Merriweather-Light",
-  CIRCULAR: "CircularStd",
-  SORAY: "SoRay"
-};
+import {Platform} from 'react-primitives';
+
+let fonts;
+
+if (Platform.OS === 'web') {
+  fonts = {
+    MERRIWEATHER: "Merriweather-Light",
+    CIRCULAR: "CircularStd",
+    SORAY: "SoRay"
+  }
+} else {
+  fonts = {
+    MERRIWEATHER: "Merriweather-Light",
+    CIRCULAR: 'CircularStd-Book',
+    SORAY: 'SoRay-ExtraBold'
+  }
+}
+
+export {fonts}
