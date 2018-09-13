862d405f025cdd2b5ae2b5610c7ee4a530084b71
Oscar Nylander <oscar@hedvig.com>
2018-09-13 11:53:29 +0200

    ts things

--- fonts.ts
+++ fonts.ts
@@ -1,18 +1,24 @@
 import { Platform } from 'react-primitives';
 
+interface Fonts {
+  MERRIWEATHER: string,
+  CIRCULAR: string,
+  SORAY: string
+}
+
 const nativeFonts = {
-  MERRIWEATHER: "Merriweather-Light",
+  MERRIWEATHER: 'Merriweather-Light',
   CIRCULAR: 'CircularStd-Book',
-  SORAY: 'SoRay-ExtraBold'
+  SORAY: 'SoRay-ExtraBold',
 }
 
 const webFonts = {
-  MERRIWEATHER: "Merriweather-Light",
-  CIRCULAR: "CircularStd",
-  SORAY: "SoRay"
+  MERRIWEATHER: 'Merriweather-Light',
+  CIRCULAR: 'CircularStd',
+  SORAY: 'SoRay'
 }
 
-const fonts = Platform.select({
+const fonts: Fonts = Platform.select({
   web: webFonts,
   android: nativeFonts,
   ios: nativeFonts
