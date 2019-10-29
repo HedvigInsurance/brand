99b258e42c9dfddff72ddb0b61090dbbe69e5672
Sam Pettersson <sam@sampettersson.com>
2019-10-29 10:11:58 +0100

    Add V2 colors

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import { colors } from './colors'
+import { colors, colorsV2 } from './colors'
 import { fonts, getCdnFontFaces } from './fonts'
 
 export {
   colors,
+  colorsV2,
   fonts,
   getCdnFontFaces
 }
