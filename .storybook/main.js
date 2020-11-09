f01a380139be14d362824966118994fab9177820
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-11-09 15:13:08 +0100

    Fix some formatting errors & export components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../components/**/*.stories.tsx'],
+  stories: ['../src/components/**/*.stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
